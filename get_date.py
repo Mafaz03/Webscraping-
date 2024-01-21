1809721d7a7f469ae91a5e9fe84a588c4dd74693
JOELvb10 <140704222+JOELvb10@users.noreply.github.com>
2024-01-21 16:50:33 +0530

    Update get_date.py
     making it more readable

--- get_date.py
+++ get_date.py
@@ -6,50 +6,39 @@ def fetch_date_from_url(url: str):
     """
     This function fetches the date from a webpage.
 
-    It prompts the user to enter a URL, sends a GET request to that URL, and attempts to find a date in the HTML content of the webpage.
-    If a date is found, it converts the date into the 'day-month-year' format and prints it; if not, it prints 'Date not found'.
-    If an error occurs during the process, it prints 'An error occurred' along with the error message.
+    It sends a GET request to the provided URL, retrieves the HTML content, and attempts to find a date using the htmldate library.
+    If a date is found, it converts the date into the 'day-month-year' format and returns it; if not, it returns None.
+    If an error occurs during the process, it prints an error message and returns None.
 
     Args:
-    url (str) = string of url needed to find date of.
+    url (str): The URL of the webpage to fetch the date from.
 
     Returns:
-    formatted_date(Datetime) = date of article (if exist)
+    formatted_date (str): The date of the article in 'dd-mm-yyyy' format, or None if no date is found or an error occurs.
     """
-    
     try:
+        # Send a GET request to the URL and retrieve the HTML content
         html = requests.get(url).content.decode('utf-8')
+        
+        # Attempt to find a date in the HTML content
         date = find_date(html)
+        
         if date is None:
-            # print("Date not found")
+            # If no date is found, return None
             return None
         else:
             # Convert the date string into a datetime object
             date_obj = datetime.strptime(date, "%Y-%m-%d")
             # Convert the datetime object into the desired format
             formatted_date = date_obj.strftime("%d-%m-%Y")
-
-            # data={
-            #     url:url,
-            #     date:formatted_date
-            # }
-            # writeone
             
+            # Return the formatted date
             return formatted_date
     except Exception as e:
-        
-        # data={
-            #     url:url,
-            #     date:formatted_date
-            # }
-            # writeone
-        
+        # Print an error message and return None in case of an error
         print("An error occurred:", str(e))
         return None
     
-    
-"""
-## Example Usecase:
-url = "https://constructionreviewonline.com/concrete/how-permanent-christmas-lights-can-transform-your-phoenix-home/"
-print(fetch_date_from_url(url = url))
-"""
+# Example usage:
+# url = "https://constructionreviewonline.com/concrete/how-permanent-christmas-lights-can-transform-your-phoenix-home/"
+# print(fetch_date_from_url(url=url))
