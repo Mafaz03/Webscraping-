7d98d433d85bc0c1fdf7bd79c8b59f3a07b42189
Mohamed Mafaz <mohdmafaz200303@gmail.com>
2024-02-25 23:10:54 +0530

    Update api_keys.py

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,6 @@
 open_ai_key_list = ["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",
-                   "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"]
+                   "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",
+                   ""]
 
 serp_key_list = ["915c3b9eda34b8454f0b932407e7fe42944d8c80537a3e76ebbf94d0f0f115c8"
                  ,"d7555dcb6698cf4004b757eeade1ac86a425b101cb972691dc757282f353d9f6",] # Mafaz and Hamdan
