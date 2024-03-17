de1e4bc8c1bd692104ab100587d93303035283c8
Mohamed Mafaz <mohdmafaz200303@gmail.com>
2024-03-17 17:49:31 +0530

    Create progress.py

---
+++ progress.py
@@ -0,0 +1,10 @@
+def progress_bar_once(word, percentage = None, num = 30, ascii = "┄", title = False):
+    
+    if title:
+        S1 = word.center(num, "=")
+        return S1
+    else:
+        S1 = word.center(num, ascii)
+        if percentage: return "├" + S1 + "→  " + str(percentage) + '%'
+        else:
+            return "├" + S1 + "→  "
